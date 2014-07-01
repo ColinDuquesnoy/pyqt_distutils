02737f94f2a75d8199359cd773ccfdf3224308d4
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-07-01 15:00:02 +0200

    Add classifiers

--- setup.py
+++ setup.py
@@ -23,5 +23,22 @@ setup(
     author='Colin Duquesnoy',
     author_email='colin.duquesnoy@gmail.com',
     description='A set of distutils extension for building Qt ui files',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Setuptools Plugin',
+        'Environment :: Console',
+        'Environment :: X11 Applications :: Qt',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: Microsoft',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: System :: Software Distribution',
+        'Topic :: Text Editors :: Integrated Development Environments (IDE)'
+    ],
     entry_points={'console_scripts': ['pyuicfg = pyqt_distutils.pyuicfg:main']}
 )
